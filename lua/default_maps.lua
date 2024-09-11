6af4451d3de8340ae9c06fd46d5c648f8f5962b7
secretdeveloperisme <linh072217@gmail.com>
2024-09-11 22:15:14 +0700

    fix: add resizewindows key maps

--- lua/default_maps.lua
+++ lua/default_maps.lua
@@ -1,7 +1,7 @@
 -- 📂lua/🌑maps.lua
 
 local function map(mode, lhs, rhs, desc)
-  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc})
+  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
 end
 
 -- Save
@@ -26,3 +26,9 @@ map("n", "<C-h>", "<C-w>h")
 map("n", "<C-l>", "<C-w>l")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-j>", "<C-w>j")
+
+-- Resize Windows
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
